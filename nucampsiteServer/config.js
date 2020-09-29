fe6fef9d5e2ec8919429e382f2c44f16bb3336f4
Nick Marofsky <marofskyn@gmail.com>
2020-09-29 17:54:29 -0400

    Passport Facebook

--- nucampsiteServer/config.js
+++ nucampsiteServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '680875045867535',
+        clientSecret: '88745ea2fa43c7c47f156e9c4f59e774'
+    }
 }
